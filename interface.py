01386799419f06582d1c4cd9844ac5485bc2f450
Ayush Ray <i.ayushray95@gmail.com>
2019-02-05 17:50:02 -0700

    Created range partition module for generating horizontal fragments of the entire ratings table categorized based on the rating scale.

--- interface.py
+++ interface.py
@@ -41,6 +41,54 @@ def loadratings(ratingstablename, ratingsfilepath, openconnection):
     pass
 
 
+def rangepartition(ratingstablename, numberofpartitions, openconnection):
+    #sets a upper bound for each range
+    rating_range = 5.0 / numberofpartitions
+
+    range = rating_range
+
+    partitions = numberofpartitions
+
+    count = 0
+
+    cursor = openconnection.cursor()
+
+    i = 0
+    while i < numberofpartitions:
+
+        #create as many range partitions as are the number of partitions
+        cursor.execute('DROP TABLE IF EXISTS range_part' + str(i))
+        query4 = "CREATE TABLE range_part" + str(i) + "(UserID int, MovieID int, Rating float)"
+
+        cursor.execute(query4)
+
+        if i==0:
+            query6 ="INSERT INTO range_part" + str(i) + "(UserID, MovieID, Rating)" + "SELECT * FROM " + ratingstablename + " WHERE Rating" + "<=" + str(rating_range)+ " AND Rating" + ">=" + str(count)
+            cursor.execute(query6)
+
+        else:
+            query6 = "INSERT INTO range_part" + str(i) + "(UserID, MovieID, Rating)" + "SELECT * FROM " + ratingstablename + " WHERE Rating" + "<=" + str(rating_range) + " AND Rating" + ">" + str(count)
+            cursor.execute(query6)
+
+
+        count = rating_range
+        rating_range += range
+
+        i += 1
+
+    #stores the upper bound for range and the number of partitions to be used for range insertion
+    cursor.execute('DROP TABLE IF EXISTS PART')
+    cursor.execute('CREATE TABLE PART (RatingRange int, Numberofpartitions int)')
+    cursor.execute('INSERT INTO PART (RatingRange, Numberofpartitions) VALUES (' + str(range) + ', ' + str(partitions) + ')')
+
+    openconnection.commit()
+
+    cursor.close()
+
+    pass
+
+
+
 # Function calls
 create_db("trial_a1_1")
 con = getopenconnection()
