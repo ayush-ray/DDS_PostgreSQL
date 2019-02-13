44d3b8f190734b1ce3d46c4a1c1faa3290265d10
Ayush Ray <i.ayushray95@gmail.com>
2019-02-13 16:45:47 -0700

    updated interface

--- interface.py
+++ interface.py
@@ -9,6 +9,25 @@ def getopenconnection(user='postgres', password='1234', dbname='postgres'):
     return psycopg2.connect("dbname='" + dbname + "' user='" + user + "' host='localhost' password='" + password + "'")
 
 
+def create_db(dbname):
+    # Connect to the default database
+    con = getopenconnection(dbname='postgres')
+    con.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = con.cursor()
+
+    # Check if an existing database with the same name exists
+    cur.execute('SELECT COUNT(*) FROM pg_catalog.pg_database WHERE datname=\'%s\'' % (dbname,))
+    count = cur.fetchone()[0]
+    if count == 0:
+        cur.execute('CREATE DATABASE %s' % (dbname,))  # Create the database
+    else:
+        print 'A database named {0} already exists'.format(dbname)
+
+    # Clean up
+    cur.close()
+    con.close()
+
+
 def loadratings(ratingstablename, ratingsfilepath, openconnection):
     cur = openconnection.cursor()
     ratings = open(ratingsfilepath, 'r')
