78c5f1ff4994639ae013811a5ca10718142e13a2
Ayush Ray <i.ayushray95@gmail.com>
2019-02-21 12:46:05 -0700

    Created file for adding modules that can query DB fragments that were created to access data from fragments

---
+++ Fragment_Querying.py
@@ -0,0 +1,11 @@
+import psycopg2
+import os
+import sys
+
+
+def RangeQuery(ratingMinValue, ratingMaxValue, openconnection, outputPath):
+	pass
+
+
+def PointQuery(ratingValue, openconnection, outputPath):
+	pass
