342fdc89f5bc6e3477963e6920326740a6597d42
Ayush Ray <i.ayushray95@gmail.com>
2019-03-06 23:46:35 -0700

    added functionality for round robin partitions

--- Fragment_Querying.py
+++ Fragment_Querying.py
@@ -18,7 +18,15 @@ def RangeQuery(ratingMinValue, ratingMaxValue, openconnection, outputPath):
         for j in rangepart_for_output:
             if j[2] <= ratingMaxValue and j[2] >= ratingMinValue:
                 var_file.write("RangeRatingsPart" + str(i) + "," + str(j[0]) + "," + str(j[1]) + "," + str(j[2]) + "\n")
-    
+    # getting the count of roundrobin table created by Assignment1 in DB
+    cur.execute("SELECT COUNT(*) FROM pg_stat_user_tables where relname like 'roundrobinratingspart%'")
+    metadata_rrobin = cur.fetchall()
+    for i in range(metadata_rrobin[0][0]):
+        cur.execute("SELECT * FROM RoundRobinRatingsPart"+str(i))
+        rrobinpart_for_output = cur.fetchall()
+        for j in rrobinpart_for_output:
+            if ratingMinValue <= j[2] <= ratingMaxValue:
+                var_file.write("RoundRobinRatingsPart"+str(i)+","+str(j[0])+","+str(j[1])+","+str(j[2])+"\n")
 
     var_file.close()
     cur.close()
